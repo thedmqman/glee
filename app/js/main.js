476744951114cf55034aeca4f91e9ce0e1e693ad
thedmqman <thedmqman@ukr.net>
2021-12-13 23:34:48 +0200

    added product-list-1 page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function () {
 
+  $('.filter-recent__stars').rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.01,
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+  
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true,
@@ -7,19 +28,20 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000
   });
-
+  
   var mixer = mixitup('.products__items',{
     selectors: {
-      target: '.products__item',
+      target: '.products-item--filter',
       control: '.filter-1'
     }
   });
-
+  
   var mixer = mixitup('.new__items',{
     selectors: {
       target: '.new__item',
       control: '.filter-2'
     }
   });
-
+  
+  
 });
